71a4dec564a8085fde4b577f85890c9435402dcc
pixeldreamdev <pixeldreamdev@outlook.com>
2025-09-13 19:37:50 -0300

    feat: ajustado a rota de login

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,7 +2,6 @@
 import { Toaster } from "react-hot-toast";
 import Footer from "../components/Footer";
 import TopHeader from "../components/TopHeader";
-import "../globals.css";
 
 export default function LoginLayout({
   children,
@@ -10,41 +9,39 @@ export default function LoginLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
-      <body className="min-h-screen flex flex-col bg-gray-100">
-        <TopHeader sidebarCollapsed={false} />
-        <main className="flex-1 min-w-0 transition-all duration-300 w-full">
-          {children}
-        </main>
-        <Footer />
-        <Toaster
-          position="top-right"
-          toastOptions={{
+    <>
+      <TopHeader sidebarCollapsed={false} />
+      <main className="flex-1 min-w-0 transition-all duration-300 w-full">
+        {children}
+      </main>
+      <Footer />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          loading: {
+            duration: Infinity,
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: "#4CAF50",
+              secondary: "#fff",
+            },
+          },
+          error: {
             duration: 4000,
-            style: {
-              background: "#363636",
-              color: "#fff",
+            iconTheme: {
+              primary: "#f44336",
+              secondary: "#fff",
             },
-            loading: {
-              duration: Infinity,
-            },
-            success: {
-              duration: 3000,
-              iconTheme: {
-                primary: "#4CAF50",
-                secondary: "#fff",
-              },
-            },
-            error: {
-              duration: 4000,
-              iconTheme: {
-                primary: "#f44336",
-                secondary: "#fff",
-              },
-            },
-          }}
-        />
-      </body>
-    </html>
+          },
+        }}
+      />
+    </>
   );
 }
